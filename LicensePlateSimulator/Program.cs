701e8e60cebe560f6edf2b7f413b692aa03df802
Steef-Jan Wiggers <steefjan@msn.com>
2021-02-28 10:52:15 +0100

    First version

---
+++ LicensePlateSimulator/Program.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace LicensePlateSimulator
+{
+    internal class Program
+    {
+        //Service bus connection string
+        private const string ServiceBusConnectionString = "<service bus connection string>";
+        //Name of the queue 
+        private const string QueueName = "<queue name>";
+        //Folder where to find the license plates
+        private const string FolderPath = @"<Your file location; path>";
+
+
+        private static void Main()
+        {
+            Console.WriteLine("======================================================");
+            Console.WriteLine("License Plate Simulator");
+            Console.WriteLine("======================================================");
+
+            //Process
+            ProcessAsync().GetAwaiter().GetResult();
+
+            //Wait
+            Thread.Sleep(5000);
+
+            //Read results
+            GetMessage();
+            GetMessage();
+            GetMessage();
+            GetMessage();
+            GetMessage();
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit.");
+            Console.WriteLine("======================================================");
+
+            Console.ReadKey();
+        }
+
+
+
+        private static async Task ProcessAsync()
+        {
+            // Retrieve storage account from connection string.
+            var storageAccount = CloudStorageAccount.Parse(
+                "DefaultEndpointsProtocol=https;AccountName=licenseplatesstorage;AccountKey=EAMIik/gc+nhU/M72joRrLDi8FBeBmb2dOdEOyeCm8qLyg4zlrKk7zKjVircKmgmKKeeRimvNPx1lXhiME0KRw==;EndpointSuffix=core.windows.net");
+
+            // Create the blob client.
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve reference to a previously created container.                                    
+            var container = blobClient.GetContainerReference("images");
+
+            await container.CreateIfNotExistsAsync();
+
+            // Set the permissions so the blobs are public. 
+            var permissions = new BlobContainerPermissions
+            {
+                PublicAccess = BlobContainerPublicAccessType.Blob
+            };
+            await container.SetPermissionsAsync(permissions);
+
+            //read license plates from folder
+            foreach (var file in Directory.EnumerateFiles(FolderPath, "*.png"))
+            {
+                var fileName = file.Replace(FolderPath, "");
+                var blockBlob = container.GetBlockBlobReference(fileName);
+                await blockBlob.UploadFromFileAsync(file);
+
+                Console.WriteLine($"Upload file : {fileName} to blob container images");
+
+            }
+        }
+
+        private static void GetMessage()
+        {
+            var queueClient = QueueClient.CreateFromConnectionString(ServiceBusConnectionString, QueueName);
+
+            var message = queueClient.Receive();
+            var stream = message.GetBody<Stream>();
+            var reader = new StreamReader(stream);
+            var s = reader.ReadToEnd();
+
+            Console.WriteLine($"License : {s}");
+
+            message.Complete();
+        }
+
+    }
+}
---
+++ LicensePlateSimulator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LicensePlateSimulator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LicensePlateSimulator")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7a37c4e9-1e68-4f71-8b51-ae02bceb9f5b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
